c5807f258d254b03ce657d01a2903fb0824bad40
Alexander Popov <alexander.popov233@gmail.com>
2024-07-10 10:01:40 +0200

    health

---
+++ health.php
@@ -0,0 +1,37 @@
+<?php
+	//error_reporting(E_ALL);
+	//ini_set('display_errors', '1');
+  require('danger.php');
+	echo "<!DOCTYPE HTML><html lang='en'><head>";
+?>
+		<meta charset="utf-8"/>
+		<title>PDO Database connection</title>
+		<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	</head>
+	<body>
+									
+		<div class="container">
+			<h2>Dangers</h2>
+			<table class="table table-striped table-hover">
+				<thead>
+					<tr>
+						<th>Id</th>
+						<th>Description</th>
+						<th>Danger</th>
+						<th>Image</th>
+						<th>Longitude</th>
+						<th>Latitude</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+						echo 'hi'
+					?>
+				</tbody>
+			</table>
+      <h1>Hi</h1>
+		</div>
+	</body>
+</html>
